0ca43675289f90f3b35f189026ac0c41f8ab6041
chriswtodd <18588248+chriswtodd@users.noreply.github.com>
2025-05-25 17:28:00 +1200

    fix: Sidemenu filters dont expand (#42)

---
+++ src/models/filters.ts
@@ -0,0 +1,11 @@
+interface Filter {
+    Key: string
+    Name: string
+}
+
+export const EngineClasses:Filter = {Key: "EngineClasses", Name: "Engine Classes"}
+export const EmissionType:Filter = {Key: "EmissionType", Name: "Emission Type"}
+export const Granularity:Filter = {Key: "Granularity", Name: "Granularity"}
+export const TripsBetween:Filter = {Key: "TripsBetween", Name: "Trips Between"}
+export const StreamType:Filter = {Key: "StreamType", Name: "Stream Type"}
+export const Routes:Filter = {Key: "Routes", Name: "Routes"}
